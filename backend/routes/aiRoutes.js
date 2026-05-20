8b240501a98e7de7247dce956daea44d28c8b8b2
Subhasis Dash <dashsubhasis95@gmail.com>
2026-05-20 19:37:57 +0530

    added ai routes.js router

---
+++ backend/routes/aiRoutes.js
@@ -0,0 +1,72 @@
+// routes/aiRoutes.js
+
+import express from "express";
+
+import {
+  classifyWaste,
+  getPredictionHistory,
+  getPredictionById,
+  deletePrediction,
+  getAIAnalytics,
+} from "../controllers/aiController.js";
+
+import {
+  protect,
+  adminOnly,
+} from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+/*
+    @route   POST /api/ai/classify
+*/
+
+router.post(
+  "/classify",
+  protect,
+  classifyWaste
+);
+
+/*
+    @route   GET /api/ai/history
+*/
+
+router.get(
+  "/history",
+  protect,
+  getPredictionHistory
+);
+
+/*
+    @route   GET /api/ai/analytics
+*/
+
+router.get(
+  "/analytics",
+  protect,
+  adminOnly,
+  getAIAnalytics
+);
+
+/*
+    @route   GET /api/ai/:id
+*/
+
+router.get(
+  "/:id",
+  protect,
+  getPredictionById
+);
+
+/*
+    @route   DELETE /api/ai/:id
+*/
+
+router.delete(
+  "/:id",
+  protect,
+  adminOnly,
+  deletePrediction
+);
+
+export default router;
